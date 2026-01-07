7c6084661a2f843c0255e0ef3161cf510df288bf
imededdine BENZARTI <imededdine.benzarti@rnta.local>
2026-01-07 15:22:30 +0100

    video 25

---
+++ attendanceserver/src/main/java/com/attendanceserver/services/AdminService.java
@@ -0,0 +1,48 @@
+package com.attendanceserver.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.attendanceserver.DTO.UserDTO;
+import com.attendanceserver.entities.Project;
+import com.attendanceserver.entities.User;
+import com.attendanceserver.repository.ProjectRepository;
+import com.attendanceserver.repository.UserRepository;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+public class AdminService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public UserDTO createUser(UserDTO userDTO){
+        boolean exists = userRepository.findByEmail(userDTO.getEmail()).isPresent();
+        if(exists)
+        {
+            throw new EntityExistsException("user already exists");
+        }
+
+        Optional<Project> optionalProject= projectRepository.findById(userDTO.getProjectID());
+        if(optionalProject.isPresent())
+        {
+            User user = new User();
+            user.setUserRole(userDTO.getUserRole());
+            user.setEmail(userDTO.getEmail());
+            user.setName(userDTO.getName());
+            user.setPassword(userDTO.getPassword());
+            user.setProject(optionalProject.get());
+
+            User userCreated = userRepository.save(user);
+
+            return userCreated.getUserDTO();
+        }
+
+        throw new EntityNotFoundException("Project not found");
+    }
+}
